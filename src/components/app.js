e67228dbfa6f079aed75396ecd14581dea5a5163
Jianan Fang <fjn19971007@outlook.com>
2018-09-18 00:40:41 -0500

    upload files

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import SearchBar from './containers/search_bar';
-import WeatherList from './containers/weather_list';
-import './App.css';
 
-class App extends Component {
+import SearchBar from '../containers/search_bar';
+import WeatherList from '../containers/weather_list';
+
+export default class App extends Component {
   render() {
     return (
       <div>
@@ -14,5 +14,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReduxPromise from 'redux-promise';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
